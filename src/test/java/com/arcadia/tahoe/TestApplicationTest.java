343d883b232feb3234b95af742cd5ae11f44e446
dmkolomiiets <dmytro.kolomiiets@vitechteam.com>
2024-03-29 20:13:10 +0200

    initial

--- src/test/java/com/arcadia/tahoe/TestApplicationTest.java
+++ src/test/java/com/arcadia/tahoe/TestApplicationTest.java
@@ -1,11 +1,13 @@
 package com.arcadia.tahoe;
 
+import io.cucumber.junit.CucumberOptions;
 import io.cucumber.junit.platform.engine.Cucumber;
 import io.cucumber.spring.CucumberContextConfiguration;
 import io.fabric8.kubernetes.client.DefaultKubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClient;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cache.CacheManager;
@@ -18,6 +20,28 @@ import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
   webEnvironment = SpringBootTest.WebEnvironment.NONE)
 @CucumberContextConfiguration
 @Cucumber
+@CucumberOptions(
+  glue= {"stepDefinitions"},
+  monochrome = true,
+
+  plugin = {
+    "pretty","summary",
+    "json:target/cucumber-reports/Cucumber.json",
+    "junit:target/cucumber-reports/Cucumber.xml",
+    "html:target/cucumber-reports",
+//    "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter",
+  }
+//  monochrome = true
+
+//  plugin = {
+//    "pretty",
+//    "junit:target/cucumber-reports/Cucumber.xml",
+//    "json:target/cucumber-reports/Cucumber.json",
+//    "html:target/cucumber-reports/Cucumber.html",
+//
+//
+//  },
+)
 class TestApplicationTest {
 
   @Autowired
